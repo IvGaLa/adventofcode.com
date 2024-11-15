2c18d19588691f49cad59425da29f74fffcba7fb
Ivan GaLa <github@laodesign.es>
2024-11-15 23:08:18 +0100

    day 2 part two passed

--- 2023/lib.js
+++ 2023/lib.js
@@ -2,5 +2,5 @@ import fs from 'fs'
 
 export const _readInput = (_fileInput) => {
   const _data = fs.readFileSync(_fileInput, 'utf-8')
-  return _data.split('\n').filter(line => line.trim().length > 0)
+  return _data.split('\n').filter(line => line.trim().length > 0).map(line => line.trim())
 }
