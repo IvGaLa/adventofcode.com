a4aa7a09f65c2c8e9d9ca6528cc3f8d5a32502a9
Ivan GaLa <github@laodesign.es>
2024-11-18 18:24:06 +0100

    day 3 part two passed.

--- 2023/day03/index.js
+++ 2023/day03/index.js
@@ -27,6 +27,37 @@ Here is an example engine schematic:
 In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
 
 Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
+
+
+
+--- Part Two ---
+The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+
+You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+
+Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+
+The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+
+This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+
+Consider the same engine schematic again:
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
+
+What is the sum of all of the gear ratios in your engine schematic?
+
+
 */
 
 import { _readInput } from "../lib.js";
@@ -110,12 +141,66 @@ const day03 = () => {
 
 }
 
+//console.log(`Result: ${day03()}`); // Result: 549908
+
+/******************************************************************
+************************* Part two ********************************
+*******************************************************************/
+
+const day03b = () => {
+  const _fileInput = './2023/day03/inputb.txt'
+  //const _fileInput = './2023/day03/example.txt' // 4361
+  const input = _readInput(_fileInput)
+  let rows = input.length;
+  let cols = input[0].length;
+
+  const gearsDic = {};
+
+  const findGears = (str, num, i, j) => {
+    j = j === -1 ? 0 : j;
+    for (let k = 0; k < str.length; k++) {
+      const ch = str.charAt(k);
+      if (ch === "*") {
+        const ind = `${i}-${j + k}`;
+        gearsDic[ind] = gearsDic[ind]
+          ? [...gearsDic[ind], parseInt(num)]
+          : [parseInt(num)];
+      }
+    }
+  };
+
+  for (let i = 0; i < rows; i++) { // filas
+    for (let j = 0; j < cols; j++) { // caracteres
+      const n = "" + input[i][j];
+      if (isNaN(n)) continue;
+
+      let num = n;
+      while (++j < cols) {
+        if (Number.isInteger(parseInt(input[i][j]))) num += input[i][j];
+        else break;
+      }
+
+      const top =
+        i === 0 ? "" : input[i - 1].substring(j - num.length - 1, j + 1);
+      const btm =
+        i === rows - 1 ? "" : input[i + 1].substring(j - num.length - 1, j + 1);
+      const lft = input[i][j - num.length - 1] || "";
+      const rgt = input[i][j] || "";
+
+      findGears(top, num, i - 1, j - num.length - 1);
+      findGears(btm, num, i + 1, j - num.length - 1);
+      findGears(lft, num, i, j - num.length - 1);
+      findGears(rgt, num, i, j);
+    }
+  }
+
+  const v = Object.values(gearsDic)
+    .filter((x) => x.length === 2)
+    .map((x) => x[0] * x[1])
+    .reduce((a, x) => a + x, 0);
+
+  return v
+}
 
 
-
-console.log(`Result: ${day03()}`); // Result: 549908
-
-
-
-
-
+console.log(`Result: ${day03b()}`); // Result: 81166799
