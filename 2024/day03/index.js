9aa6ca6c0dc3bcb2fe471c293b9ae14e94a467e2
Ivan GaLa <github@laodesign.es>
2024-12-03 20:25:40 +0100

    day 03/2024 part 2 passed

--- 2024/day03/index.js
+++ 2024/day03/index.js
@@ -16,6 +16,26 @@ xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 Only the four highlighted sections are real mul instructions. Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).
 
 Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
+
+
+--- Part Two ---
+As you scan through the corrupted memory, you notice that some of the conditional statements are also still intact. If you handle some of the uncorrupted conditional statements in the program, you might be able to get an even more accurate result.
+
+There are two new instructions you'll need to handle:
+
+The do() instruction enables future mul instructions.
+The don't() instruction disables future mul instructions.
+Only the most recent do() or don't() instruction applies. At the beginning of the program, mul instructions are enabled.
+
+For example:
+
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+This corrupted memory is similar to the example from before, but this time the mul(5,5) and mul(11,8) instructions are disabled because there is a don't() instruction before them. The other mul instructions function normally, including the one at the end that gets re-enabled by a do() instruction.
+
+This time, the sum of the results is 48 (2*4 + 8*5).
+
+Handle the new instructions; what do you get if you add up all of the results of just the enabled multiplications?
+
 */
 
 import { _readInput } from '../../lib.js'
@@ -46,7 +66,44 @@ const day03 = (fileInput) => {
   return count // 181345830
 }
 
+
+// -------------------------------------------------------------
+
+
+const getDoDontMules = (_data) => {
+  const mules = _data.match(/mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/g);
+  let isEnabled = true;
+  let count = 0
+
+  for (const mul of mules) {
+    if (mul === 'do()') {
+      isEnabled = true
+    } else if (mul === "don't()") {
+      isEnabled = false
+    } else if (isEnabled) {
+      const numbers = getMulNumbers(mul).map(Number)
+      count += numbers[0] * numbers[1]
+    }
+  }
+
+  return count;
+}
+
+const day03Two = (fileInput) => {
+  const _data = _readInput(fileInput)
+
+  let input = ''
+  for (const line of _data) {
+    input += line
+  }
+
+  return getDoDontMules(input)
+}
+
+
 //const fileInput = './2024/day03/example.txt'
+//const fileInput = './2024/day03/example2.txt'
 const fileInput = './2024/day03/input.txt'
 
-console.log(day03(fileInput));
+console.log(day03(fileInput)); // 181345830
+console.log(day03Two(fileInput)); // 98729041
