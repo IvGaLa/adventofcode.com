f09fe6c804c600c59bf22a44cbada4154a76795a
Ivan GaLa <github@laodesign.es>
2024-12-05 18:56:53 +0100

    day 05/2024 part 2 passed

--- 2024/day05/index.js
+++ 2024/day05/index.js
@@ -1,45 +1,4 @@
 /*
---- Day 5: Print Queue ---
-Satisfied with their search on Ceres, the squadron of scholars suggests subsequently scanning the stationery stacks of sub-basement 17.
-
-The North Pole printing department is busier than ever this close to Christmas, and while The Historians continue their search of this historically significant facility, an Elf operating a very familiar printer beckons you over.
-
-The Elf must recognize you, because they waste no time explaining that the new sleigh launch safety manual updates won't print correctly. Failure to update the safety manuals would be dire indeed, so you offer your services.
-
-Safety protocols clearly indicate that new pages for the safety manuals must be printed in a very specific order. The notation X|Y means that if both page number X and page number Y are to be produced as part of an update, page number X must be printed at some point before page number Y.
-
-The Elf has for you both the page ordering rules and the pages to produce in each update (your puzzle input), but can't figure out whether each update has the pages in the right order.
-
-For example:
-
-47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47
 The first section specifies the page ordering rules, one per line. The first rule, 47|53, means that if an update includes both page number 47 and page number 53, then page number 47 must be printed at some point before page number 53. (47 doesn't necessarily need to be immediately before 53; other pages are allowed to be between them.)
 
 The second section specifies the page numbers of each update. Because most safety manuals are different, the pages needed in the updates are different too. The first update, 75,47,61,53,29, means that the update consists of page numbers 75, 47, 61, 53, and 29.
@@ -76,9 +35,23 @@ Determine which updates are already in the correct order. What do you get if you
 
 To begin, get your puzzle input.
 
+
+--- Part Two ---
+While the Elves get to work printing the correctly-ordered updates, you have a little time to fix the rest of them.
+
+For each of the incorrectly-ordered updates, use the page ordering rules to put the page numbers in the right order. For the above example, here are the three incorrectly-ordered updates and their correct orderings:
+
+75,97,47,61,53 becomes 97,75,47,61,53.
+61,13,29 becomes 61,29,13.
+97,13,75,29,47 becomes 97,75,47,29,13.
+After taking only the incorrectly-ordered updates and ordering them correctly, their middle page numbers are 47, 29, and 47. Adding these together produces 123.
+
+Find the updates which are not in the correct order. What do you get if you add up the middle page numbers after correctly ordering just those updates?
+
 */
 
 import { _readInput } from '../../lib.js'
+
 
 const getRulesUpdates = (_data) => {
   const rules = []
@@ -105,6 +78,7 @@ const day05 = (fileInput) => {
   const { rules, updates } = getRulesUpdates(_data)
 
   let count = 0
+
   for (const update of updates) {
     if (isValidUpdate(update, rules)) {
       const middlePage = Math.floor(update.length / 2)
@@ -115,7 +89,89 @@ const day05 = (fileInput) => {
   return count
 }
 
+
+// --------------------------------------------------------
+
+
+const sortInvalid = (r, u) => {
+  let total = 0
+
+  const updates = []
+  const rules = []
+  u.forEach((v) => { updates.push(v) })
+  r.sort().forEach((v) => { rules.push(v) })
+
+  updates.forEach((update) => {
+    let isCorrect = true
+    for (let i = 0; i < rules.length; i++) {
+      const [xIndex, yIndex] = rules[i]
+      const first = update.indexOf(xIndex)
+      const second = update.indexOf(yIndex)
+
+      if ((first !== -1 && second !== -1) && (second < first)) {
+        const v1 = update[first]
+        const v2 = update[second]
+        update[first] = v2
+        update[second] = v1
+
+        isCorrect = false
+        i = 0
+      }
+    }
+
+    if (!isCorrect) {
+      const middle = Math.floor(update.length / 2)
+      total += parseInt(update[middle])
+    }
+  })
+
+  return total
+}
+
+
+
+
+const day05Two = (fileInput) => {
+  const _data = _readInput(fileInput)
+  const { rules: r, updates: u } = getRulesUpdates(_data)
+  let count = 0
+
+  const updates = []
+  const rules = []
+  u.forEach((v) => { updates.push(v) })
+  r.sort().forEach((v) => { rules.push(v) })
+
+  updates.forEach((update) => {
+    let isCorrect = true
+    for (let i = 0; i < rules.length; i++) {
+      const [xIndex, yIndex] = rules[i]
+      const first = update.indexOf(xIndex)
+      const second = update.indexOf(yIndex)
+
+      if ((first !== -1 && second !== -1) && (second < first)) {
+        const v1 = update[first]
+        const v2 = update[second]
+        update[first] = v2
+        update[second] = v1
+
+        isCorrect = false
+        i = 0
+      }
+    }
+
+    if (!isCorrect) {
+      const middle = Math.floor(update.length / 2)
+      count += parseInt(update[middle])
+    }
+  })
+
+  return count
+}
+
+
+
 //const fileInput = './2024/day05/example.txt'
 const fileInput = './2024/day05/input.txt'
 
 console.log(day05(fileInput)); // 3608
+console.log(day05Two(fileInput)); // 4922
